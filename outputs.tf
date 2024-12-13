493fa74a375776dc6d4555901b8930726d6050c8
Abigail Ranson <abby@abbyranson.com>
2024-12-13 13:32:28 -0800

    Fix outputs

--- outputs.tf
+++ outputs.tf
@@ -1,6 +1,5 @@
 output "zone_id" {
-  description = "The ID of the created DNS zone"
-  value       = google_dns_managed_zone.primary.id
+  value = google_dns_managed_zone.primary.managed_zone_id
 }
 
 output "zone_name" {
@@ -8,7 +7,7 @@ output "zone_name" {
   value       = google_dns_managed_zone.primary.name
 }
 
-output "name_servers" {
+output "nameservers" {
   description = "The list of nameservers that should be used to configure the domain"
   value       = google_dns_managed_zone.primary.name_servers
 }
@@ -19,6 +18,11 @@ output "dns_records" {
     for record in google_dns_record_set.records :
     trimsuffix(record.name, ".") => record.rrdatas[0]
   }
+}
+
+
+output "dns_name" {
+  value = local.dns_name
 }
 
 output "domain" {
